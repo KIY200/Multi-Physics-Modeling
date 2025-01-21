dc66a6fee9db43982ecbf39b558eab0f5a7ff28c
Kim, Inyong <kiminy@oregonstate.edu>
2025-01-21 12:35:39 -0800

    figure added

--- Matlab model/iteration_WEC_array.m
+++ Matlab model/iteration_WEC_array.m
@@ -21,9 +21,32 @@ V_WEC1_max = max(out.logsout{4}.Values);
 RAO(ii-9,1)=wave_period;
 RAO(ii-9,2)=V_WEC1_max/V_s_max;
 end
-figure
-plot(RAO(:,1),RAO(:,2))
-grid on
-xlabel("Wave Period (seconds)")
-ylabel("RAO")
-ylim([0,2])
+
+%% plotting
+% Open the figure file
+fig = openfig("FloatHeaveRAOOneBodyHeaveOnlyExpTNumBEM_20231004_112907_legend edit.fig");
+
+% Get current axes of the figure
+ax = gca;
+
+% hold(ax, 'on');
+% % Clear existing plots on the same axes (optional)
+% delete(findobj(ax, 'Type', 'Line', 'DisplayName', 'Eq-ckt')); % Remove any existing 'Eq-ckt' line
+% 
+% eq_ckt = plot(RAO(:,1),RAO(:,2), 'DisplayName', 'Eq-ckt'); % Add DisplayName for legend
+% 
+% hold(ax, 'off');
+lines = findobj(ax, 'Type', 'Line');
+
+set(lines(1), 'LineWidth', 1.5, 'LineStyle','-','Color','r');
+% Release hold
+
+
+% Set the window size and position
+% Position = [left, bottom, width, height]
+
+% grid on
+% xlabel("Wave Period (seconds)")
+% ylabel("RAO")
+% ylim([0,2])
+
