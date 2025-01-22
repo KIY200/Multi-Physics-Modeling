66b0c2afdea6e4166b9eab3bcf7d304b334d8a6b
Kim, Inyong <kiminy@oregonstate.edu>
2025-01-22 12:45:23 -0800

    Figure_update

--- Matlab model/iteration_WEC_array.m
+++ Matlab model/iteration_WEC_array.m
@@ -5,7 +5,7 @@ for ii = 10:50
 wave_period = 0.1*ii;
 run("wec_array_init.mlx")
 out=sim("wec_array.slx");
-filename=sprintf("sim_out/WEC_array_apf/T%d.mat",wave_period*100);
+filename=sprintf("sim_out/WEC_array_apf/delay_T%d.mat",wave_period*100);
 save(filename,"out")
 clear
 end
@@ -13,7 +13,7 @@ end
 RAO = zeros(41,2);
 for ii = 10:50
     wave_period = 0.1*ii;
-filename=sprintf("sim_out/WEC_array_apf/T%d.mat",wave_period*100);
+filename=sprintf("sim_out/WEC_array_apf/delay_T%d.mat",wave_period*100);
 load(filename)
 V_s_max = max(out.logsout{1}.Values);
 V_WEC1_max = max(out.logsout{4}.Values);
@@ -21,32 +21,22 @@ V_WEC1_max = max(out.logsout{4}.Values);
 RAO(ii-9,1)=wave_period;
 RAO(ii-9,2)=V_WEC1_max/V_s_max;
 end
-
+plot(RAO(:,1),RAO(:,2))
 %% plotting
 % Open the figure file
-fig = openfig("FloatHeaveRAOOneBodyHeaveOnlyExpTNumBEM_20231004_112907_legend edit.fig");
+fig = openfig("FloatHeaveRAOOneBodyHeaveOnlyExpTNumBEMEqCkt_20250122.fig");
 
 % Get current axes of the figure
 ax = gca;
-
-% hold(ax, 'on');
+% lines = findobj(ax, 'Type', 'Line');
+% delete(lines(1))
+hold(ax, 'on');
 % % Clear existing plots on the same axes (optional)
 % delete(findobj(ax, 'Type', 'Line', 'DisplayName', 'Eq-ckt')); % Remove any existing 'Eq-ckt' line
 % 
-% eq_ckt = plot(RAO(:,1),RAO(:,2), 'DisplayName', 'Eq-ckt'); % Add DisplayName for legend
+eq_ckt = plot(RAO(:,1),RAO(:,2),'-o','Color','r','markersize',8,'DisplayName', 'Eq-ckt','linewidth',1.5); % Add DisplayName for legend
 % 
-% hold(ax, 'off');
-lines = findobj(ax, 'Type', 'Line');
-
-set(lines(1), 'LineWidth', 1.5, 'LineStyle','-','Color','r');
-% Release hold
+hold(ax, 'off');
 
 
-% Set the window size and position
-% Position = [left, bottom, width, height]
-
-% grid on
-% xlabel("Wave Period (seconds)")
-% ylabel("RAO")
-% ylim([0,2])
 
