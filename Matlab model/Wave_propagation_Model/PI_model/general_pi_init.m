3aef0865f6598f44e133b789bc283d4041acf223
Kim, Inyong <kiminy@oregonstate.edu>
2025-05-21 17:53:11 -0700

    SIL update

--- Matlab model/Wave_propagation_Model/PI_model/general_pi_init.m
+++ Matlab model/Wave_propagation_Model/PI_model/general_pi_init.m
@@ -1,4 +1,7 @@
-% general pi model init
+%% House keeping
+clear;clc;close all;
+
+%% general pi model init
 f=0.1;
 T=10; % 10 seconds
 t_stop=T*3;
@@ -7,21 +10,31 @@ g = 9.81;
 w = 2*pi*f;
 k = w^2/g;
 L_wave = 2*pi/k;
+% c=3e8; % light speed
+% L_light = c*T;
+H = 2;
 
-distance=100;
-del_theta = k*distance*2*pi;
+distance=7.5;
+del_angle = distance*k *180/pi;
+d_eta_RMS = H/2 * 2*pi / T /sqrt(2);
+P_wave = 1/32/pi * rho * g^2 * 10 * H^2; % Power flux per width (W/m)
+SIL = d_eta_RMS^2/P_wave;  % SIL = sqrt(L/C)
+% LC = w^2/g^2
+% L/C = SIL^2
+% L^2 = w^2/g^2*SIL^2
+L = distance * w/g*SIL;
+C = distance * w/g/SIL/2;
 
-% SIL = 0.01;  % SIL = sqrt(L/C)
-% C = distance * w/g/2;
-% L = distance * w/g * SIL^2;
-C = 1.97e3/2;
-L = 5.21e-5;
 
-SIL = sqrt(L/C);
+% C = 1.97e3/2;
+% L = 5.21e-5;
+
+% SIL = sqrt(L/C);
 
 %%
 Simout=sim("general_pi_model.slx");
 %%
+
 t = Simout.tout;
 V0=Simout.V0.data;
 [idx_V0,~]=find(V0==max(V0(1:200)));
